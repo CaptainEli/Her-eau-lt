073b45fdfebcfdaa2a47a6f860da07e3060fda72
CaptainEli <wicked.amy.steiner@gmail.com>
2025-12-02 17:05:49 +0100

    Create Open files.R

---
+++ Open files.R
@@ -0,0 +1,7 @@
+# Libraries
+install.packages("raster")
+install.packages("rgdal")
+
+# Load packages
+library(raster)
+df2015 <- raster("C:/Documents/Etudes/M2&DOC~1/Doctorat/WORKSH~1/HACKAT~1/HACKAT~1/data/PRADJU~1.NC")
