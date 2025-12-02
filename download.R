3de11a52c0f1ba9696541bed7703bde95cec4992
ggenelot <77398544+ggenelot@users.noreply.github.com>
2025-12-02 17:19:19 +0100

    Added list of files

--- download.R
+++ download.R
@@ -1,13 +1,13 @@
-urls <- c(
-  "https://object.files.data.gouv.fr/meteofrance-drias/SocleM-Climat-2025/CPRCM/METROPOLE/ALPX-3/CNRM-ESM2-1/r1i1p1f2/CNRM-AROME46t1/ssp370/1hr/prAdjust/version-hackathon-102025/prAdjust_FR-Metro_CNRM-ESM2-1_ssp370_r1i1p1f2_CNRM-MF_CNRM-AROME46t1_v1-r1_MF-CDFt-COMEPHORE-ALPX-3-1997-2020_1hr_201601010030-201612312330.nc",
-  "https://object.files.data.gouv.fr/meteofrance-drias/SocleM-Climat-2025/CPRCM/METROPOLE/ALPX-3/CNRM-ESM2-1/r1i1p1f2/CNRM-AROME46t1/ssp370/1hr/prAdjust/version-hackathon-102025/prAdjust_FR-Metro_CNRM-ESM2-1_ssp370_r1i1p1f2_CNRM-MF_CNRM-AROME46t1_v1-r1_MF-CDFt-COMEPHORE-ALPX-3-1997-2020_1hr_204201010030-204212312330.nc",
-  "https://example.com/image.png"
-)
+# Read URLs from file (ignores blank lines and lines starting with #)
+urls_file <- "liste_fichier_2042_2061.txt"
+if (!file.exists(urls_file)) stop("URL list file not found: ", urls_file)
 
-dest_dir <- "data/downloads"
+urls <- readLines(urls_file, warn = FALSE, encoding = "UTF-8")
+urls <- trimws(urls)
+urls <- urls[urls != "" & !grepl("^\\s*#", urls)]
+if (length(urls) == 0) stop("No URLs found in ", urls_file)
 
-if (!dir.exists(dest_dir)) dir.create(dest_dir, recursive = TRUE)
-
+dest_dir <- "data"
 
 download_files <- function(urls, dest_dir) {
   if (!dir.exists(dest_dir)) dir.create(dest_dir, recursive = TRUE)
@@ -17,23 +17,13 @@ download_files <- function(urls, dest_dir) {
     dest_path <- file.path(dest_dir, fname)
     message("Downloading ", u, " -> ", dest_path)
     tryCatch(
-      {
-        download.file(u, destfile = dest_path, mode = "wb", quiet = FALSE)
-      },
+      download.file(u, destfile = dest_path, mode = "wb", quiet = FALSE),
       error = function(e) message("Failed to download ", u, ": ", e$message)
     )
   }
 }
 
-for (u in urls) {
-  fname <- basename(u)
-  dest_path <- file.path(dest_dir, fname)
-  message("Downloading ", u, " -> ", dest_path)
-  tryCatch(
-    download.file(u, destfile = dest_path, mode = "wb", quiet = FALSE),
-    error = function(e) message("Failed to download ", u, ": ", e$message)
-  )
-}
+download_files(urls, dest_dir)
 
 
 
