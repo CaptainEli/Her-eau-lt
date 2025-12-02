da8e00ddda6d804d2f98d41d71e25d58858f297c
ggenelot <77398544+ggenelot@users.noreply.github.com>
2025-12-02 17:50:04 +0100

    Add a shapefile for herault + created a script to process

--- cdo_processing.sh
+++ cdo_processing.sh
@@ -1,8 +1,8 @@
-
-
 chemin="data"
-file=
-chemin_out=
-output_file=
+file="C:\Users\ggenelot\Nouveau dossier\Hackathon-climat-github\data\prAdjust_FR-Metro_CNRM-ESM2-1_ssp370_r1i1p1f2_CNRM-MF_CNRM-AROME46t1_v1-r1_MF-CDFt-COMEPHORE-ALPX-3-1997-2020_1hr_201601010030-201612312330.nc"
+chemin_out="data\processed"
+output_file="output.nc"
 
-cdo timmean ${chemin}/${file} ${chemin_out}/${output_file} #moyenne sur tous les pas de temps
+
+cdo -f nc -setmissval,0 -remapnn,${chemin}/${file} -shp2grid,data/shape/departements-20140306-100m.shp herault_mask.nc
+cdo -O -ifthen herault_mask.nc ${chemin}/${file} ${chemin_out}/${output_file}
